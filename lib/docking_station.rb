e840ff4cd863c0d89be835cffd19f9d15b9defb8
jinimcoroneo <jinim1983@gmail.com>
2017-06-07 22:34:42 +0100

    specifying the capacity of the docking stations

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,16 +2,20 @@ require "./lib/bike.rb"
 
 class DockingStation
 
-  attr_reader :bike
+  attr_reader :bikes
+
+  def initialize
+  	@bikes = []
+  end
   
   def release_bike
-  	raise "There are no available bikes" unless @bike
-  	@bike
+  	raise "There are no available bikes" unless @bikes.size >= 1
+  	@bikes.pop
   end
 
   def dock_bike(bike)
-  	raise "Station full, cannot dock bike" if @bike
-  	@bike = bike
+  	raise "Station full, cannot dock bike" if @bikes.size >= 20
+  	@bikes << bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,26 +3,33 @@ require "./lib/docking_station.rb"
 describe DockingStation do 
 
   it "releases working bikes" do
-  	bike = Bike.new
-  	subject.dock_bike(bike)
-  	bike = subject.release_bike
+    bike = Bike.new
+    subject.dock_bike(bike)
+    subject.release_bike
   	expect(bike).to be_working
   end
 
   describe "#dock_bike" do
     it "docks a bike" do
     	bike = Bike.new
-    	expect(subject.dock_bike(bike)).to eq bike
+    	subject.dock_bike(bike)
+      expect(subject.bikes).to include(bike)
     end
 
-    it "raises an error when there is already a docked bike" do
-      bike = Bike.new
-      subject.dock_bike(bike)
-      expect{ subject.dock_bike(bike) }.to raise_error "Station full, cannot dock bike"
+    it "raises an error when the station is at capacity (20 bikes)" do
+      20.times { subject.dock_bike(Bike.new) }
+      expect{ subject.dock_bike(Bike.new) }.to raise_error "Station full, cannot dock bike"
     end  
   end
 
   describe "#release_bike" do
+    it "releases a bike" do
+      bike = Bike.new
+      subject.dock_bike(bike)
+      subject.release_bike
+      expect(subject.bikes).not_to include(bike)
+    end
+
   	it "does not release a bike if there are none available" do
   	  expect { subject.release_bike }.to raise_error("There are no available bikes")
   	end
